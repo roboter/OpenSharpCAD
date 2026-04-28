e9b9c771eee1a428b6c21d248cf3ff28fa64e10d
roboter <robby.roboter@gmail.com>
2026-04-28 16:41:37 +0300

    Update macOS GLFW startup and pin agg-sharp submodule to local fix

--- OpenSharpCAD/Program.cs
+++ OpenSharpCAD/Program.cs
@@ -4,22 +4,57 @@ using System.Runtime.InteropServices;
 using System.Globalization;
 using System.Threading;
 using System.IO;
+using System.Reflection;
 using MatterHackers.Agg.UI;
+using MatterHackers.VectorMath;
 
 namespace CSharpCAD;
 
+public class MacOsInformationProvider : IOsInformationProvider
+{
+    public OSType OperatingSystem => OSType.Mac;
+
+    public MatterHackers.Agg.Point2D DesktopSize => new MatterHackers.Agg.Point2D(1920, 1080); // Default size
+
+    public long PhysicalMemory => 8L * 1024L * 1024L * 1024L; // 8GB default
+}
+
 class Program
 {
+    private static void EnsureGlfwProviderLoaded()
+    {
+        var candidatePaths = new[]
+        {
+            Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Submodules", "agg-sharp", "Glfw", "bin", "Debug", "net6.0", "MatterHackers.GlfwProvider.dll"),
+            Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Submodules", "agg-sharp", "Glfw", "bin", "Release", "net6.0", "MatterHackers.GlfwProvider.dll"),
+            Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Submodules", "agg-sharp", "Glfw", "bin", "Debug", "net6.0-windows", "MatterHackers.GlfwProvider.dll"),
+            Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Submodules", "agg-sharp", "Glfw", "bin", "Release", "net6.0-windows", "MatterHackers.GlfwProvider.dll")
+        };
+
+        foreach (var path in candidatePaths)
+        {
+            var fullPath = Path.GetFullPath(path);
+            if (File.Exists(fullPath))
+            {
+                Assembly.LoadFrom(fullPath);
+                return;
+            }
+        }
+    }
+
     [STAThread]
     static void Main(string[] args)
     {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             AggContext.Config.ProviderTypes.SystemWindowProvider = "MatterHackers.Agg.UI.OpenGLWinformsWindowProvider, agg_platform_win32";
+            AggContext.Config.ProviderTypes.OsInformationProvider = "MatterHackers.Agg.Platform.WinformsInformationProvider, agg_platform_win32";
         }
         else
         {
+            EnsureGlfwProviderLoaded();
             AggContext.Config.ProviderTypes.SystemWindowProvider = "MatterHackers.GlfwProvider.GlfwWindowProvider, MatterHackers.GlfwProvider";
+            AggContext.OsInformation = new MacOsInformationProvider();
         }
 
         try
